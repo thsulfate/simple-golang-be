0832117f32494a9806b57c2809324995a77445c6
Linux User <thsulfate@TSF-LAB-01.>
2024-05-30 03:56:23 +0000

    edit main and gitignore

--- main.go
+++ main.go
@@ -100,8 +100,8 @@ func main() {
 	http.HandleFunc("/healthcheck", healthCheckHandler)
 
 	// Start the server
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting server on :8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatalf("Could not start server: %s\n", err.Error())
 	}
 }
